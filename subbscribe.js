bdc282b80b5a8566e95768035426495f329985eb
Shlomi Nissan <contact@snissan.me>
2015-06-28 15:14:02 +1000

    Added delay property to dispatch slide-in after a set number of seconds

--- subbscribe.js
+++ subbscribe.js
@@ -12,15 +12,18 @@
 
         // Default settings
         var settings = $.extend({
+
 	    list	 : 'MailChimp',
-            url          : '',
-            title        : 'Never miss a post!',
-            text         : 'Get our latest posts and announcements in your inbox. You won\'t regret it!',
-            name         : 'Subbscribe',
-            color        : '#ee6262',
-            thumbnail    : 'https://s3-ap-southeast-2.amazonaws.com/subbscribe/img/avatar.png',
-            emailonly	 : false,
-            cm_mail_field   : '',
+            url           : '',
+            title         : 'Never miss a post!',
+            text          : 'Get our latest posts and announcements in your inbox. You won\'t regret it!',
+            name          : 'Subbscribe',
+            color         : '#ee6262',
+            thumbnail     : 'https://s3-ap-southeast-2.amazonaws.com/subbscribe/img/avatar.png',
+            emailonly	    : false,
+            cm_mail_field : '',
+            delay         : 0,
+
 	}, options);
 
         // Make sure a URL has been passed through
@@ -30,7 +33,7 @@
             return;
 
         };
-        
+
         //make sure the cm_mail_field is set when using Campaign Monitor
         if( settings.list === 'CampaignMonitor' && !settings.cm_mail_field.length ){
 
@@ -38,7 +41,7 @@
             return;
 
         }
-	
+
 	var _name 	= '';
 	var _email 	= '';
 	var _url 	= '';
@@ -47,7 +50,7 @@
 	// Change field names if yours don’t match
 
 	if( settings.list == 'MailChimp' ) {
-		
+
 		_name 	= 'NAME';
 		_email 	= 'EMAIL';
 		_action	= settings.url.replace('/post?', '/post-json?').concat('&c=?');
@@ -66,13 +69,13 @@
 		return;
 
 	}
-	
+
 	// Separate the input fields from the HTML
 	// if emailonly is set, nameInput should be blank
 
 	var nameInput 	= '';
 	var emailInput 	= '<input type="email" name="' + _email + '" id="subb-EMAIL" placeholder="Email Address" />';
-	
+
 	if( !settings.emailonly ) {
 
 		nameInput = ' <input type="text" name="' + _name + '" id="subb-NAME" placeholder="Name" />';
@@ -81,13 +84,19 @@
 
 
 	// HTML
-        var html = '<div id="subbscribe"> <div class="subb-title">' + settings.title + ' <img class="close-x" src="https://s3-ap-southeast-2.amazonaws.com/subbscribe/img/close.svg" />  </div> <div class="subb-body"> <div class="subb-hidden"> <div class="subb-thumbnail"> <img style="width: 40px; height: 40px;" src="' + settings.thumbnail + '" /> </div> <div class="subb-hidden"> <div class="subb-site"> &nbsp;' + settings.name + ' </div> <button class="subb-button show-form">Subscribe</button> </div> </div> <div class="subb-form" style="display: none"> <p>' + settings.text + '</p> <form id="mc-embedded-subbscribe-form" method="post" action="' + settings.url + '"> <div class="subbscribe-alert subbscribe-error" style="display: none">Oops! Check your details and try again.</div> <div class="subbscribe-alert subbscribe-success" style="display: none">Thanks! Check your email for confirmation.</div> <div class="text-input"> ' + nameInput + ' </div> <div class="text-input"> ' + emailInput + ' </div> <button class="subb-button submit-form" type="submit" style="width: 100%; margin-bottom: 10px;">Subscribe</button></form> <div class="footer">Powered by <a href="http://www.subbscribe.com" target="_blank">Subbscribe.com</a></div> </div> </div> </div>';
+        var html = '<div id="subbscribe" style="display: none"><div class="subb-title">' + settings.title + ' <img class="close-x" src="https://s3-ap-southeast-2.amazonaws.com/subbscribe/img/close.svg" />  </div> <div class="subb-body"> <div class="subb-hidden"> <div class="subb-thumbnail"> <img style="width: 40px; height: 40px;" src="' + settings.thumbnail + '" /> </div> <div class="subb-hidden"> <div class="subb-site"> &nbsp;' + settings.name + ' </div> <button class="subb-button show-form">Subscribe</button> </div> </div> <div class="subb-form" style="display: none"> <p>' + settings.text + '</p> <form id="mc-embedded-subbscribe-form" method="post" action="' + settings.url + '"> <div class="subbscribe-alert subbscribe-error" style="display: none">Oops! Check your details and try again.</div> <div class="subbscribe-alert subbscribe-success" style="display: none">Thanks! Check your email for confirmation.</div> <div class="text-input"> ' + nameInput + ' </div> <div class="text-input"> ' + emailInput + ' </div> <button class="subb-button submit-form" type="submit" style="width: 100%; margin-bottom: 10px;">Subscribe</button></form> <div class="footer">Powered by <a href="http://www.subbscribe.com" target="_blank">Subbscribe.com</a></div> </div> </div> </div>';
 
         if(getCookie('subbscribe-hidden') != 1) {
 
             this.append(html);
-            $('#subbscribe').css('width', $('.subb-site').width() + 200 );
-	    $('#subbscribe').addClass('animated slideInRight');
+
+             setTimeout(function(){
+
+               $('#subbscribe').css('display', 'block');
+               $('#subbscribe').css('width', $('.subb-site').width() + 200 );
+               $('#subbscribe').addClass('animated slideInRight');
+
+             }, settings.delay * 1000);
 
         }
 
@@ -111,7 +120,7 @@
             });
 
             settings.onClose.call();
-            
+
         });
 
         $('#subbscribe .show-form').click(function(){
@@ -137,26 +146,26 @@
                     data: $(this).serialize(),
                     dataType: 'json',
                     contentType: "application/json; charset=utf-8",
-                    
+
                     success: function (data) {
-			
+
                        if ( isError(data) ) {
 
                            	console.log('Subbscribe Error: submission failed.');
 
-                       } 
+                       }
 		       else {
 
                             //SUCCESS
                             resetFormFields()
                             $('.subbscribe-success').slideDown();
-                            
+
                             setTimeout(function(){ $('#subbscribe').addClass('animated fadeOut'); }, 2000);
                             $('#subbscribe').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-                
+
                                 $('#subbscribe').remove();
                                 setCookie('subbscribe-hidden', 1, 365); // Hide for a year
-                                
+
                                 if(typeof settings.onSubbscribe === 'function'){
                                     settings.onSubbscribe.call();
                                 }
@@ -181,9 +190,9 @@
           Helpers
         ===============================================================================
         */
-	
+
 	function isError(data) {
-		
+
 		console.log( data );
 
 		if ( settings.list == 'MailChimp' ) {
@@ -204,7 +213,7 @@
 				return true;
 
 			}
-			
+
 			return false;
 
 		}
@@ -212,7 +221,7 @@
 		return true;
 
 	}
-		
+
         function resetFormFields() {
 
             $('#subbscribe input').each(function(){
@@ -235,7 +244,7 @@
             var email   = $('#subb-EMAIL');
 
 	    if( !settings.emailonly ) {
-		
+
 		if( name.val().length < 2 ) {
 
                     valid = false;
@@ -248,7 +257,7 @@
                 }
 
 	    }
-            
+
             if ( !validateEmail( email.val() ) ) {
 
                 valid = false;
@@ -263,14 +272,14 @@
             return valid;
 
         }
-	
+
         function setCookie(cname, cvalue, exdays) {
-            
+
             var d = new Date();
             d.setTime(d.getTime() + (exdays*24*60*60*1000));
             var expires = "expires="+d.toUTCString();
             document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
-            
+
         }
 
         function getCookie(cname) {
